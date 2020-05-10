3e165476c6c86c644ac7c556d0419c52844432ab
Vladislav Serebrennikov <brainvlad@gmail.com>
2020-05-10 12:31:12 +0300

    Clean up a bit and add comments

--- expr.cpp
+++ expr.cpp
@@ -19,21 +19,16 @@
 #include <clang/Frontend/CompilerInvocation.h>
 #include <clang/Frontend/TextDiagnosticPrinter.h>
 #include <llvm/ADT/IntrusiveRefCntPtr.h>
-#include <llvm/ADT/SmallString.h>
 #include <llvm/ADT/SmallVector.h>
-#include <llvm/ADT/StringRef.h>
-#include <llvm/ADT/Triple.h>
 #include <llvm/ExecutionEngine/Orc/ExecutionUtils.h>
 #include <llvm/ExecutionEngine/Orc/LLJIT.h>
 #include <llvm/ExecutionEngine/Orc/ThreadSafeModule.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 #include <llvm/Option/Option.h>
-#include <llvm/Support/Debug.h>
 #include <llvm/Support/DynamicLibrary.h>
 #include <llvm/Support/Host.h>
 #include <llvm/Support/ManagedStatic.h>
-#include <llvm/Support/raw_ostream.h>
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/Support/VirtualFileSystem.h>
 
@@ -49,14 +44,9 @@ struct Exprcpp_data {
     std::unique_ptr<llvm::orc::LLJIT> jit;
 };
 
-std::string get_executable_path(const char* argv_0, void* main_addr) {
-  return llvm::sys::fs::getMainExecutable(argv_0, main_addr);
-}
-
-llvm::ExitOnError ExitOnErr;
-
 static void VS_CC exprcpp_init(VSMap* in, VSMap* out, void** instance_data,
-                               VSNode* node, VSCore* core, const VSAPI* vsapi) {
+                               VSNode* node, VSCore* core, const VSAPI* vsapi)
+{
     const auto data{static_cast<Exprcpp_data*>(*instance_data)};
     vsapi->setVideoInfo(data->src_a_info, 1, node);
 }
@@ -66,7 +56,8 @@ static const VSFrameRef *VS_CC exprcpp_get_frame(int n, int activationReason,
                                                  void** frame_data,
                                                  VSFrameContext* frame_ctx,
                                                  VSCore* core,
-                                                 const VSAPI* vsapi) {
+                                                 const VSAPI* vsapi)
+{
     const auto data{static_cast<Exprcpp_data*>(*instance_data)};
 
     if (activationReason == arInitial) {
@@ -106,7 +97,8 @@ static const VSFrameRef *VS_CC exprcpp_get_frame(int n, int activationReason,
 }
 
 static void VS_CC exprcpp_free(void* instance_data, VSCore* core,
-                               const VSAPI* vsapi) {
+                               const VSAPI* vsapi)
+{
     const auto data{static_cast<Exprcpp_data*>(instance_data)};
     vsapi->freeNode(data->src_a_node);
     vsapi->freeNode(data->src_b_node);
@@ -115,7 +107,8 @@ static void VS_CC exprcpp_free(void* instance_data, VSCore* core,
 }
 
 static void VS_CC exprcpp_create(const VSMap* in, VSMap* out, void* userData,
-                                 VSCore* core, const VSAPI* vsapi) {
+                                 VSCore* core, const VSAPI* vsapi)
+{
     const auto data{new Exprcpp_data{vsapi->propGetNode(in, "clip_a", 0, 0),
                                      vsapi->propGetNode(in, "clip_b", 0, 0),
                                      vsapi->propGetData(in, "code",   0, 0)}};
@@ -125,120 +118,95 @@ static void VS_CC exprcpp_create(const VSMap* in, VSMap* out, void* userData,
 
     // LLVM stuff
 
-    llvm::DebugFlag = true;
-    llvm::setCurrentDebugType("orc");
-    // This just needs to be some symbol in the binary; C++ doesn't
-    // allow taking the address of ::main however.
-    auto main_addr{reinterpret_cast<void*>(get_executable_path)};
-    std::string path{get_executable_path(nullptr, main_addr)};
+    // Load user code into in-memory virtual FS.
+    llvm::IntrusiveRefCntPtr<llvm::vfs::InMemoryFileSystem> vfs{
+        new llvm::vfs::InMemoryFileSystem{}};
+    vfs->addFile("expr.cpp", std::time(nullptr),
+                 llvm::MemoryBuffer::getMemBuffer(data->source_code));
+
+    // Create diagnostics engine for driver.
     llvm::IntrusiveRefCntPtr<clang::DiagnosticOptions> diag_opts{
         new clang::DiagnosticOptions()};
     clang::TextDiagnosticPrinter* diag_client{
         new clang::TextDiagnosticPrinter{llvm::errs(), &*diag_opts}};
-
-    llvm::IntrusiveRefCntPtr<clang::DiagnosticIDs> diag_id{new clang::DiagnosticIDs()};
+    llvm::IntrusiveRefCntPtr<clang::DiagnosticIDs> diag_id{
+        new clang::DiagnosticIDs()};
     clang::DiagnosticsEngine diags{diag_id, &*diag_opts, diag_client};
 
-    const std::string triple_str{llvm::sys::getProcessTriple()};
-    llvm::Triple triple{triple_str};
+    // Create driver.
+    clang::driver::Driver driver{"clang", llvm::sys::getProcessTriple(), diags,
+                                 vfs};
 
-    llvm::IntrusiveRefCntPtr<llvm::vfs::InMemoryFileSystem> vfs{
-        new llvm::vfs::InMemoryFileSystem{}};
-    vfs->addFile("expr.cpp", std::time(nullptr), llvm::MemoryBuffer::getMemBuffer(data->source_code));
-
-    clang::driver::Driver driver{path, triple.str(), diags, vfs};
-    driver.setCheckInputsExist(false);
-
-    // FIXME: This is a hack to try to force the driver to do something we can
-    // recognize. We need to extend the driver library to support this use model
-    // (basically, exactly one input, and the operation mode is hard wired).
-    // SmallVector<const char *, 16> Args(argv, argv + argc);
+    // Create compilation using driver.
     llvm::SmallVector<const char*, 3> args{"clang", "expr.cpp", "-fsyntax-only"};
-    // Args.push_back("-fsyntax-only");
     std::unique_ptr<clang::driver::Compilation> compilation{
         driver.BuildCompilation(args)};
-    if (!compilation)
+    if (!compilation) {
+        vsapi->setError(out, "Failed to create compilation");
         return;
+    }
 
-    // We expect to get back exactly one command job, if we didn't something
-    // failed. Extract that job from the compilation.
+    // Extract arguments from compilation.
     const clang::driver::JobList& jobs{compilation->getJobs()};
     if (jobs.size() != 1 || !clang::isa<clang::driver::Command>(*jobs.begin())) {
-        llvm::SmallString<256> msg;
-        llvm::raw_svector_ostream os{msg};
-        jobs.Print(os, "; ", true);
-        diags.Report(clang::diag::err_fe_expected_compiler_job) << os.str();
-        vsapi->setError(out, os.str().data());
+        vsapi->setError(out, "Failed to create job list");
         return;
     }
-
     const clang::driver::Command& cmd{
         llvm::cast<clang::driver::Command>(*jobs.begin())};
-    if (llvm::StringRef(cmd.getCreator().getName()) != "clang") {
-        diags.Report(clang::diag::err_fe_expected_clang_command);
-        return;
-    }
-
-    // Initialize a compiler invocation object from the clang (-cc1) arguments.
     const llvm::opt::ArgStringList& cc_args{cmd.getArguments()};
     auto ci{std::make_unique<clang::CompilerInvocation>()};
     clang::CompilerInvocation::CreateFromArgs(*ci, cc_args, diags);
 
-    // Show the invocation, with -v.
-    if (ci->getHeaderSearchOpts().Verbose) {
-        llvm::errs() << "clang invocation:\n";
-        jobs.Print(llvm::errs(), "\n", true);
-        llvm::errs() << "\n";
-    }
-
-    // Create a compiler instance to handle the actual work.
+    // Create compiler instance to do the work.
     clang::CompilerInstance clang;
     clang.setInvocation(std::move(ci));
     clang.createFileManager(vfs);
-
-    // Create the compilers actual diagnostics engine.
+    if (!clang.hasFileManager()) {
+        vsapi->setError(out, "Failed to create file manager from virtual FS");
+        return;
+    }
     clang.createDiagnostics();
-    std::cout << 31 << std::endl;
     if (!clang.hasDiagnostics()) {
-        std::cout << 32 << std::endl;
-        vsapi->setError(out, "Can't create diagnostics engine");
+        vsapi->setError(out, "Failed to create diagnostics engine");
         return;
     }
 
-    // Infer the builtin include path if unspecified.
-    if (clang.getHeaderSearchOpts().UseBuiltinIncludes &&
-        clang.getHeaderSearchOpts().ResourceDir.empty())
-        clang.getHeaderSearchOpts().ResourceDir =
-        clang::CompilerInvocation::GetResourcesPath(nullptr, main_addr);
-
-    // Create and execute the frontend to generate an LLVM bitcode module.
+    // Emit LLVM IR and extract mangled function name
     std::unique_ptr<clang::CodeGenAction> act{new clang::EmitLLVMOnlyAction{}};
     if (!clang.ExecuteAction(*act)) {
-        vsapi->setError(out, "Can't emit LLVM bitcode");
+        vsapi->setError(out, "Failed to emit LLVM bitcode");
         return;
     }
-
-    llvm::InitializeNativeTarget();
-    llvm::InitializeNativeTargetAsmPrinter();
-
     std::unique_ptr<llvm::LLVMContext> ctx{act->takeLLVMContext()};
     std::unique_ptr<llvm::Module> module{act->takeModule()};
-
     if (!module) {
-        vsapi->setError(out, "Unable to create module");
+        vsapi->setError(out, "Failed to create module");
+        return;
     }
-
     std::string func_name{[&module]{
         for (auto& v: *module) {
             if (v.hasName()) {
                 return v.getName().data();
             }
         }
-        // throw "No user functions found";
+        // throw
     }()};
 
-    auto jit{ExitOnErr(llvm::orc::LLJITBuilder().create())};
+    // Initialize backend
+    llvm::InitializeNativeTarget();
+    llvm::InitializeNativeTargetAsmPrinter();
+    auto jit{[]() {
+        if (auto expected{llvm::orc::LLJITBuilder().create()}; expected) {
+            return std::move(*expected);
+        } else {
+            // throw
+        }
+    }()};
 
+    // Add generator to JIT to resolve symbols from C and C++ runtimes
+    // referenced in user code (i.e. printf), and prevent any loaded libraries
+    // from unloading.
     auto psg{llvm::orc::DynamicLibrarySearchGenerator::GetForCurrentProcess(
         jit->getDataLayout().getGlobalPrefix())};
     if (!psg) {
@@ -248,17 +216,35 @@ static void VS_CC exprcpp_create(const VSMap* in, VSMap* out, void* userData,
     jit->getMainJITDylib().addGenerator(std::move(*psg));
     llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
 
-    auto ts_module{llvm::orc::ThreadSafeModule{std::move(module), std::move(ctx)}};
-    ExitOnErr(jit->addIRModule(std::move(ts_module)));
+    // Add IR module to JIT and get the symbol of user function.
+    auto ts_module{llvm::orc::ThreadSafeModule{std::move(module),
+                                               std::move(ctx)}};
+    if (jit->addIRModule(std::move(ts_module))) {
+        vsapi->setError(out, "Failed to add IR module to JIT");
+        return;
+    }
+    const auto symbol{[&jit, &func_name]() {
+        if (auto expected{jit->lookupLinkerMangled(jit->getMainJITDylib(), func_name)};
+            expected)
+        {
+            return *expected;
+        } else {
+            // throw
+        }
+    }()};
 
-    auto s{ExitOnErr(jit->lookupLinkerMangled(jit->getMainJITDylib(), func_name))};
-    data->user_func = reinterpret_cast<int (*)(int, int)>(s.getAddress());
+    // Store address of user function and JIT itself in filter's data to keep
+    // function alive. getAddress() triggers compilation into native code.
+    data->user_func = reinterpret_cast<int (*)(int, int)>(symbol.getAddress());
     data->jit = std::move(jit);
 
-    vsapi->createFilter(in, out, "expr_cpp", exprcpp_init, exprcpp_get_frame, exprcpp_free, fmParallel, 0, data, core);
+    vsapi->createFilter(in, out, "expr_cpp", exprcpp_init, exprcpp_get_frame,
+                        exprcpp_free, fmParallel, 0, data, core);
 }
 
-VS_EXTERNAL_API(void) VapourSynthPluginInit(VSConfigPlugin config_func, VSRegisterFunction register_func, VSPlugin* plugin)
+VS_EXTERNAL_API(void) VapourSynthPluginInit(VSConfigPlugin config_func,
+                                            VSRegisterFunction register_func,
+                                            VSPlugin* plugin)
 {
   config_func("com.endill.ExprCpp", "endill", "C++-based Expr",
               VAPOURSYNTH_API_VERSION,1, plugin);
